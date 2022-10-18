6cc42d401603d1bc79eecc7c6bd901e6086bc0d5
Hammad Ather <hather45@gmail.com>
2022-10-18 11:07:46 -0700

    Update library.py

--- library.py
+++ library.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
 
 class MappingTransformer(BaseEstimator, TransformerMixin):
   
