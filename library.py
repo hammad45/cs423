2b95da680939b93c8241a915b54f4c87027d90f9
Hammad Ather <hather45@gmail.com>
2022-11-13 20:22:25 -0800

    Update library.py

--- library.py
+++ library.py
@@ -393,3 +393,18 @@ def threshold_results(thresh_list, actuals, predicted):
 
   fancy_df = result_df.style.format(precision=2).set_properties(**properties).set_table_styles([headers])
   return (result_df, fancy_df)
+
+def halving_search(model, grid, x_train, y_train, factor=3, scoring='roc_auc'):
+  #your code below
+  halving_cv = HalvingGridSearchCV(
+    model, grid,  #our model and the parameter combos we want to try
+    scoring=scoring,  #could alternatively choose f1, accuracy or others
+    n_jobs=-1,
+    min_resources="exhaust",
+    factor=factor,  #a typical place to start so triple samples and take top 3rd of combos on each iteration
+    cv=5, random_state=1234,
+    refit=True,  #remembers the best combo and gives us back that model already trained and ready for testing
+  )
+
+  grid_result = halving_cv.fit(x_train, y_train)
+  return grid_result
